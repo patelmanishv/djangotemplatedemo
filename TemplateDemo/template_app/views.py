5f02b02179eef00c5e7a3ac582bb865cc2b46d2b
manish patel <manish.patel@ljinstitutes.edu.in>
2023-08-24 19:33:08 +0530

    first commit

---
+++ TemplateDemo/template_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def demo_view(request):
+    context = {
+        'name': 'John',
+        'age': 25,
+        'numbers': [1, 2, 3, 4, 5],
+        'show_numbers': False,
+    }
+    return render(request, 'template_app/demo_template.html', context)
